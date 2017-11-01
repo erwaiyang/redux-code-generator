1d6042478535f7039b66ec649980cd3165ced8a0
Jean Yang <bdlas321@gmail.com>
2017-11-01 11:40:53 +0800

    test spec for upperCaseAndUnderscoreFormatter

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -5,4 +5,8 @@ describe('UPPER_CASE formatter', function() {
   it('should format "get data" to "GET_DATA"', function() {
     expect(upperCaseAndUnderscoreFormatter('get data')).to.equal('GET_DATA');
   });
+  it('should format action type composed of three or more words', function() {
+    expect(upperCaseAndUnderscoreFormatter('update user nickname')).to.equal('UPDATE_USER_NICKNAME');
+    expect(upperCaseAndUnderscoreFormatter('change password and sign in')).to.equal('CHANGE_PASSWORD_AND_SIGN_IN');
+  });
 });
